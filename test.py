cdb28b2b5d2e50eb1ce7569930259b10348581fd
aeff60 <ssbunklang@gmail.com>
2025-03-14 16:20:00 +0700

    Fix indentation error

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def say_hello():
-    print("Hello, GitHub Actions!")  # แก้ไข Indentation
+    print("Hello, GitHub Actions!")
 
 
 def say_goodbye():
-    print("Goodbye, GitHub Actions!")  # แก้ไข Indentation
+    print("Goodbye, GitHub Actions!")
